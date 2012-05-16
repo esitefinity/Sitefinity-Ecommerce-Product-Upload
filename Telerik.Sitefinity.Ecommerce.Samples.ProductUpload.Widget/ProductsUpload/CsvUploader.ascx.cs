1eeafdbab1dcc6f4fe3089b7c4b39faef9067f40
Venkata Koppaka <subbarao.cse@gmail.com>
2012-05-16 12:47:35 -0700

    Updating the sample to the latest Library API. Adding ProductVariationImport.csv file for sample CSV data.

--- Telerik.Sitefinity.Ecommerce.Samples.ProductUpload.Widget/ProductsUpload/CsvUploader.ascx.cs
+++ Telerik.Sitefinity.Ecommerce.Samples.ProductUpload.Widget/ProductsUpload/CsvUploader.ascx.cs
@@ -2,20 +2,36 @@
 // make sure you have created a folder called upload in the root of your web site
 // you must also be logged-in to the Sitefinity backend for this product importer to work. 
 // You must also have CSVHelper package installed.  Install Nuget http://nuget.codeplex.com into Visual Studio as an extension
-
+// then select Search online, and search for CSV Helper.  Install the Add-on CSVHelper into Visual Studio and build project.
+// You can read more about CSVHelper here http://nuget.org/packages/CsvHelper
 using System;
 using System.Linq;
 using System.Web.UI;
+using System.Web.UI.WebControls;
 using Telerik.Sitefinity.Samples.Ecommerce.ProductUpload.Util;
 using Telerik.Sitefinity.Samples.Ecommerce.ProductUpload;
+using Telerik.Sitefinity.Samples.Ecommerce.ProductUpload.Model;
 
 namespace SitefinityWebApp.ProductsUpload
 {
     public partial class CsvUploader : UserControl
     {
+        public void btnVariationUpload_Click(object sender, EventArgs e)
+        {
+            UploadCsvFileAndTriggerProductVariationImport();
+        }
+
+        private void UploadCsvFileAndTriggerProductVariationImport()
+        {
+            string filePath = UploadFileAndGetFilePath(FileUpload2);
+
+            UploadManager uploadManager = new UploadManager(new UploadConfig { NumberOfColumns = UploadConstants.DefaultNumberOfColumnsForProductVariationImport });
+
+            ImportStatistic statistic =  uploadManager.ImportProductsVariationsFromCsvFile(filePath);
+        }
+
         protected void Page_Load(object sender, EventArgs e)
         {
-
         }
 
         protected void btnUpload_Click(object sender, EventArgs e)
@@ -25,24 +41,24 @@ namespace SitefinityWebApp.ProductsUpload
         
         private void UploadCsvFileAndTriggerProductImport()
         {
-            string filePath = UploadFileAndGetFilePath();
+            string filePath = UploadFileAndGetFilePath(FileUpload1);
 
-            UploadManager uploadManager = new UploadManager(new UploadConfig { NumberOfColumns = 11 });
+            UploadManager uploadManager = new UploadManager(new UploadConfig { NumberOfColumns = UploadConstants.DefaultNumberOfColumnsForProductImport });
 
             uploadManager.ImportProductsFromCsvFile(filePath);
         }
   
-        private string UploadFileAndGetFilePath()
+        private string UploadFileAndGetFilePath(FileUpload fileUpload)
         {
             string folderPath = Server.MapPath("~/Upload");
 
             IoHelper.ValidateFolderExsistence(folderPath);
 
-            string filePath = folderPath + @"\" + FileUpload1.FileName.ToString();
+            string filePath = folderPath + @"\" + fileUpload.FileName.ToString();
 
             IoHelper.ValidateFileExsistence(filePath);
 
-            FileUpload1.SaveAs(filePath);
+            fileUpload.SaveAs(filePath);
 
             return filePath;
         }
